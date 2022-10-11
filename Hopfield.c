7df3479910ed6d487f9f919238c948118df92be2
andrzeng <44561881+andrzeng@users.noreply.github.com>
2022-10-10 23:41:41 -0400

    Update Hopfield.c

--- Hopfield.c
+++ Hopfield.c
@@ -1,37 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
-#include <windows.h>
-
-
-
-
-
-#define PI 3.14159265458
 
 struct  HopfieldNet{
-	int size; //Side length of each square memory
+	int size; //Length of weight vector
 	float** weights; //A 2d array containing the network's weights
 };
 
+//Load a Hopfield network from a file
 struct HopfieldNet LoadHopfield(char* filename)
 {
 	struct HopfieldNet net;
 	
 	FILE* f = fopen(filename, "r");
+	
+	//Check to ensure the file exists
 	if(f == NULL)
 	{
 		printf("Error: Cannot open %s. Please ensure the name is correct", filename);
 		return;
 	}
 	
+	//The first value in the file contains the side length of the weight matrix, which is a square matrix
 	fscanf(f, "%d", &net.size);
+	
+	//Using that length, we initialize the weights of our Hopfield network
 	net.weights = malloc(sizeof(float*) * net.size);
 	for(int i = 0; i<net.size; ++i)
 	{
 		net.weights[i] = malloc(sizeof(float) * net.size);
 	}
 	
+	//Now we scan the rest of the values in the file, which specify the connection signs and amplitudes between the network neurons
 	float weight;
 	for(int i = 0; i<net.size; ++i)
 	{
@@ -46,9 +45,12 @@ struct HopfieldNet LoadHopfield(char* filename)
 	return net;	
 }
 
-void LearnPattern(struct HopfieldNet net, char* filename, float lr)
+//Learn (i.e. "store") a pattern from a file to the network
+void LearnPattern(struct HopfieldNet *net, char* filename, float lr)
 {
 	FILE* f = fopen(filename, "r");
+	
+	//Copy the pattern to a two-dimensional array
 	int patternSize = 0;
 	fscanf(f, "%d", &patternSize);
 	int pattern[patternSize][patternSize];
@@ -64,29 +66,41 @@ void LearnPattern(struct HopfieldNet net, char* filename, float lr)
 	}
 	fclose(f);
 	
+	/*
+	Based on that array, we update the weights of the network according to the rule presented in this paper: 
+	https://www.pnas.org/doi/10.1073/pnas.79.8.2554
+	*/
 	
-	
-	
+	for(int i = 0; i<patternSize*patternSize; ++i)
+	{
+		for(int x = 0; x<patternSize*patternSize; ++x)
+		{
+			(*net).weights[i][x] += (pattern[i/patternSize][i%patternSize]*pattern[x/patternSize][x%patternSize])*lr;
+		}
+	}
 }
 
-
-void help()
+//Print weights of network
+void PrintWeights(struct HopfieldNet net)
 {
-	printf("Parameters:\ninit [d] to initialize a new memory  \nrcon [filename] to reconstruct the partial memory stored in [filename]\n ");
-	printf("Learn [file]");
+	for(int i = 0; i<net.size*net.size; ++i)	
+	{
+		printf("%f ", net.weights[i/net.size][i%net.size]);
+		if((i+1)%net.size == 0)
+		{
+			printf("\n");
+		}
+	}
 }
 
 int main(int argc, char* argv[])
 {
-	
 	struct HopfieldNet net = LoadHopfield("test.txt");
-	/*
-	if(argc <= 1)
-	{
-		help();
-	}
-	printf(argv[1]);
-	*/
 	
+	printf("Weights before training: \n");
+	PrintWeights(net);
 	
+	LearnPattern(&net, "mem1.txt", 0.01);
+	printf("\nWeights after training: \n");
+	PrintWeights(net);
 }
